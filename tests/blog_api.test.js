1cf2a0444484efa11d8746a7a3c520334c588508
Mikko Virpi <t9vimi04@students.oamk.fi>
2022-07-07 00:24:58 +0300

    tehtävä 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,31 +4,13 @@ const app = require('../app')
 const Blog = require('../models/blog')
 
 const api = supertest(app)
-
-const initialBlogs = [
-  {
-    id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger kuka',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    id: '5a422aa71b54a4356234d17f8',
-    title: 'What the f Richard',
-    author: 'Richard himself',
-    url: 'youtu.be',
-    likes: 10,
-    __v: 0
-  }
-]
+const helper = require('./test_helper')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
+  let blogObject = new Blog(helper.initialBlogs[0])
   await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
+  blogObject = new Blog(helper.initialBlogs[1])
   await blogObject.save()
 })
 
@@ -43,7 +25,7 @@ test('blogs are returned as json', async () => {
 test('all blogs has returned', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(initialBlogs.length)
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
 test('a specific blog is within the returned blogs', async () => {
@@ -52,6 +34,19 @@ test('a specific blog is within the returned blogs', async () => {
 
   expect(contents[1]).toBe('Richard himself')
 })
+
+test('blog has been added', async () => {
+  await api
+    .post('/api/blogs')
+    .send(helper.newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+})
+
+
 
 afterAll(() => {
   mongoose.connection.close()
---
+++ tests/test_helper.js
@@ -0,0 +1,38 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger kuka',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    id: '5a422aa71b54a4356234d17f8',
+    title: 'What the f Richard',
+    author: 'Richard himself',
+    url: 'youtu.be',
+    likes: 10,
+    __v: 0
+  }
+]
+
+const newBlog = new Blog ({
+  id: 'mo3090faj3098usfa0',
+  title: 'Tämä on uusi blogi',
+  author: 'Uusi Blokkaaja',
+  url: 'uusi.blokkaaja.fi',
+  likes: 1,
+  __v: 0
+})
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, newBlog, blogsInDb
+}
