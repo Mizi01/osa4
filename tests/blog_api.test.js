a69ad82685c1e40500a893fb277ba5e15c32cf7e
Mikko Virpi <t9vimi04@students.oamk.fi>
2022-07-07 18:42:11 +0300

    tehtävä 4.13

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -14,36 +14,55 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
+describe('addition of a new blog', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+  test('all blogs has returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+    const contents = response.body.map(blog => blog.author)
+
+    expect(contents[1]).toBe('Richard himself')
+  })
+
+  test('blog has been added', async () => {
+    await api
+      .post('/api/blogs')
+      .send(helper.newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  })
 })
 
-test('all blogs has returned', async () => {
-  const response = await api.get('/api/blogs')
+describe('deletion of a blog', () => {
+  test('succees with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0].id
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+    await api
+      .delete(`/api/blogs/${blogToDelete}`)
+      .expect(204)
 
-test('a specific blog is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs')
-  const contents = response.body.map(blog => blog.author)
+    const blogsAtEnd = await helper.blogsInDb()
 
-  expect(contents[1]).toBe('Richard himself')
-})
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
 
-test('blog has been added', async () => {
-  await api
-    .post('/api/blogs')
-    .send(helper.newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    const author = blogsAtEnd.map(r => r.author)
+    expect(author).not.toContain(blogToDelete.author)
+  })
 })
 
 
